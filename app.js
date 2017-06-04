4beb14e8d6d7fad6007bf9f8e8c540151dbf21dc
David Sheridan <davidsheridandsgn@gmail.com>
2017-06-04 23:16:00 +0100

    Create function to take in vales from an input and print to the DOM & to add those values to an array

---
+++ app.js
@@ -0,0 +1,30 @@
+let input = document.querySelector('input');
+let addToList = document.querySelector('button.addToList');
+
+/*
+    Empty array to hold values of input field
+    Keep as block level element as will be accessed by more than one function
+*/
+
+let randomList = [];
+
+
+//Function to take data from input form and add to list
+addToList.addEventListener('click', () =>{
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
+
+    // Take input values and add as a list item to DOM
+    li.textContent = input.value;
+    ul.appendChild(li);
+
+    // Append input value to empty array for random function
+    //TODO: Create a function for this
+    randomList.push(input.value);
+
+    // Check values are being added to the array
+    console.log(randomList);
+
+    // Empty the array after a value is entered
+    input.value = '';
+})
